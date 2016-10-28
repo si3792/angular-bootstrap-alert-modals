1b5f26ee76b36202243dc2ea01ab20f99dc6f9cb
si3792 <simo.t.iliev@gmail.com>
2016-10-28 23:02:14 +0300

    Better modal styling

--- src/angular-bootstrap-alert-modals.js
+++ src/angular-bootstrap-alert-modals.js
@@ -15,7 +15,7 @@ bootstrapAlertModals.controller('AlertModalController', [
           // Convert texts to array if it isn't
           if( !($scope.texts instanceof Array) ) {
             $scope.texts = [texts];
-          }
+          } else if ($scope.texts.length == 0) $scope.texts = "";
         }
         $scope.init();
 
@@ -44,7 +44,7 @@ bootstrapAlertModals.service('AlertModalService', function(ModalService, $q, $ti
 
     var self = this;
 
-    self.alertTemplate = '<div class=\"modal fade\" data-keyboard=\"true\" tabindex=\"-1\">\n    <div class=\"modal-dialog\">\n        <div class=\"modal-content\">\n            <div class=\"modal-header\">\n                <div class=\"modal-title text-{{type}} h1 text-center\" style=\' display: inline-flex;vertical-align: middle;align-items: center;justify-content: center;\'>\n                    <span class=\'glyphicon glyphicon-ok-sign\' ng-show=\'type == \"success\"\'><\/span>\n                    <span class=\'glyphicon glyphicon-info-sign\' ng-show=\'type == \"info\"\'><\/span>\n                    <span class=\'glyphicon glyphicon-exclamation-sign\' ng-show=\'type == \"warning\"\'><\/span>\n                    <span class=\'glyphicon glyphicon-remove-sign\' ng-show=\'type == \"danger\"\'><\/span>\n                    &nbsp{{title}}\n                <\/div>\n            <\/div>\n            <div class=\"modal-body\" ng-hide=\'texts=={}\'>\n                <p ng-repeat="line in texts track by $index">{{line}}<\/p>\n            <\/div>\n            <div class=\'modal-footer\'>\n                <div class=\'containter-fluid\'>\n                    <div class=\'col-sm-6 col-sm-offset-3\'>\n                        <button focus-me type=\"button\" class=\"btn btn-{{type}} btn-block\" ng-click=\"close()\" data-dismiss=\"modal\" id="close-button">Close<\/button>\n                    <\/div>\n                <\/div>\n            <\/div>\n        <\/div>\n    <\/div>\n<\/div>';
+    self.alertTemplate = '<div class=\"modal fade\" data-keyboard=\"true\" tabindex=\"-1\">\n    <div class=\"modal-dialog\">\n        <div class=\"modal-content\">\n            <div class=\"modal-header\">\n                <div class=\"modal-title text-{{type}} h1 text-center\" style=\' display: inline-flex;vertical-align: middle;align-items: center;justify-content: center;\'>\n                    <span class=\'glyphicon glyphicon-ok-sign\' ng-show=\'type == \"success\"\'><\/span>\n                    <span class=\'glyphicon glyphicon-info-sign\' ng-show=\'type == \"info\"\'><\/span>\n                    <span class=\'glyphicon glyphicon-exclamation-sign\' ng-show=\'type == \"warning\"\'><\/span>\n                    <span class=\'glyphicon glyphicon-remove-sign\' ng-show=\'type == \"danger\"\'><\/span>\n                    &nbsp{{title}}\n                <\/div>\n            <\/div>\n            <div class=\"modal-body h4\" ng-hide=\'texts==""\'>\n                <p ng-repeat="line in texts track by $index">{{line}}<\/p>\n            <\/div>\n            <div class=\'modal-footer\'>\n                <div class=\'containter-fluid\'>\n                    <div class=\'col-sm-6 col-sm-offset-3\'>\n                        <button focus-me type=\"button\" class=\"btn btn-{{type}} btn-block\" ng-click=\"close()\" data-dismiss=\"modal\" id="close-button">Close<\/button>\n                    <\/div>\n                <\/div>\n            <\/div>\n        <\/div>\n    <\/div>\n<\/div>';
 
     self.modalOpened = false;
 
